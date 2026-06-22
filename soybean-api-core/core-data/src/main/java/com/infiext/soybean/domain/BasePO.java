42887379ab7a774529e0e119c28f6822b8efe2a2
haibiao_gu <13812838001@163.com>
2026-06-22 15:20:16 +0800

    feat: 添加核心模块及工具类，包含数学工具、排序参数、枚举类型和请求日志过滤器

--- soybean-api-core/core-data/src/main/java/com/infiext/soybean/domain/BasePO.java
+++ soybean-api-core/core-data/src/main/java/com/infiext/soybean/domain/BasePO.java
@@ -1,6 +1,5 @@
 package com.infiext.soybean.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.infiext.soybean.enums.DelEnum;
 import com.mybatisflex.annotation.Column;
 import com.mybatisflex.annotation.Id;
@@ -10,7 +9,6 @@ import com.mybatisflex.core.keygen.KeyGenerators;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -32,8 +30,6 @@ public class BasePO<T extends Model<T>> extends Model<T> {
      * 创建时间
      */
     @Column(value = "create_time")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
     /**
      * 更新人
@@ -44,8 +40,6 @@ public class BasePO<T extends Model<T>> extends Model<T> {
      * 更新时间
      */
     @Column(value = "update_time")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;
     /**
      * 删除标识
